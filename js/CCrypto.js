4ee4f72a2f2fc40859c5b4ad7619b65e2ddbc308
artem_chesnokov <artem@chesnokov.pp.ua>
2017-03-30 13:34:19 +0300

    Encrypt and decrypt files on the client side - change FileSaver.js inclusion

--- js/CCrypto.js
+++ js/CCrypto.js
@@ -5,7 +5,7 @@ var
 	ko = require('knockout'),
 	Screens = require('%PathToCoreWebclientModule%/js/Screens.js'),
 	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-	FileSaver = require('modules/%ModuleName%/js/vendors/FileSaver.js'),
+	FileSaver = require('%PathToCoreWebclientModule%/js/vendors/FileSaver.js'),
 	StreamSaver = require('modules/%ModuleName%/js/vendors/StreamSaver.min.js'),
 	Polyfill = require('modules/%ModuleName%/js/vendors/polyfill.min.js'),
 	Browser = require('%PathToCoreWebclientModule%/js/Browser.js'),
