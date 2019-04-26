99c78ee9d312cd98c4a6701717d7a8504f470f84
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:24:41 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\CoreParanoidEncryptionWebclientPlugin;
 
 /**
+ * Paranoid Encryption module allows you to encrypt files in File module using client-based functionality only.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
