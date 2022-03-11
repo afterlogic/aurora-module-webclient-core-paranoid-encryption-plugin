430ecf79efdd6c8e70305dc0cbceb213e85eddb9
TimofeevVadim <vtimofeev223@gmail.com>
2022-03-11 18:20:12 +0300

    Added share encrypted file

---
+++ vue-mobile/files/share.js
@@ -0,0 +1,54 @@
+import notification from "src/utils/notification";
+import OpenPgp from "../../../OpenPgpMobileWebclient/vue-mobile/openpgp-helper";
+import store from "src/store";
+import Crypto from "../crypto/CCrypto";
+import _ from 'lodash'
+
+let principalsEmails = []
+let file = null
+let onContinueAction = null
+
+export const init = ({contacts, currentFile, onContinueSaving}) => {
+    principalsEmails = contacts
+    file = currentFile
+    onContinueAction = onContinueSaving
+}
+
+export const updateExtendedProps = (passPassphrase) => {
+    const currentAccountEmail = store.getters['core/userPublicId']
+    const privateKey = OpenPgp.getPrivateKeyByEmail(currentAccountEmail)
+    const publicKey = OpenPgp.getPublicKeyByEmail(currentAccountEmail)
+
+    Crypto.getEncryptedKey(file, privateKey, publicKey, currentAccountEmail, passPassphrase, null, false, principalsEmails)
+        .then( async encryptKey => {
+            if (encryptKey?.sError) {
+                notification.showError(encryptKey.sError)
+                return null
+            } else if (encryptKey) {
+                const parameters = {
+                    type: store.getters['filesmobile/currentStorage'].Type,
+                    path: store.getters['filesmobile/currentPath'],
+                    name: file.name,
+                    paranoidKey: {
+                        value: encryptKey.data,
+                        key: 'ParanoidKeyShared'
+                    },
+                }
+                const result = await store.dispatch('filesmobile/asyncUpdateExtendedProps', parameters)
+                if (result) {
+                    if (_.isFunction(onContinueAction)) {
+                        onContinueAction(true)
+                    }
+                } else {
+                    if (_.isFunction(onContinueAction)) {
+                        onContinueAction(false)
+                    }
+                }
+            }
+        })
+        .catch( err => {
+            if (_.isFunction(onContinueAction)) {
+                onContinueAction(false)
+            }
+        })
+}
