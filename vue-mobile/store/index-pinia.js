810385c70d02b6fc81063541cb26e9255aad04f9
Vasil Sokolov <vasil@afterlogic.com>
2024-02-09 21:23:25 +0200

    Removed pinia getters

--- vue-mobile/store/index-pinia.js
+++ vue-mobile/store/index-pinia.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 import state from './state'
 import actionsPinia from './actions-pinia'
-import gettersPinia from './getters-pinia'
+// import gettersPinia from './getters-pinia'
 
 export const useParanoidEncryptionStore = defineStore('ParanoidEncryptionStore', {
   state: () => (state()),
