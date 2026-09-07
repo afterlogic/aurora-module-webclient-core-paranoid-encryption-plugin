7b9fe92d7a14ff2739d3658132aade059c17b06f
sash04ek <a.ovcharov@gmail.com>
2026-09-07 11:24:13 +0300

    Remove Decorator definition from module

--- Module.php
+++ Module.php
@@ -76,14 +76,6 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
     }
 
     /**
-     * @return Module
-     */
-    public static function Decorator()
-    {
-        return parent::Decorator();
-    }
-
-    /**
      * @return Settings
      */
     public function getModuleSettings()
